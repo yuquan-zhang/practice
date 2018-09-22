c0fffe17c88c3f06176c7eb1e8142009b6352544
zhangyong <zhytwo@126.com>
2018-09-22 20:23:03 +0800

    1,practice lambda expression

--- src/algorithm/Maths.java
+++ src/algorithm/Maths.java
@@ -4,4 +4,18 @@ public class Maths {
     public static double sqrt(double y) {
         return 0;
     }
+
+    public static double pow(double x, int p) {
+        if(p == 0) return 1;
+        if(p == 1) return x;
+        if(p % 2 == 0) {
+            return pow(x, p/2) * pow(x, p/2);
+        }else {
+            return pow(x, p/2) * pow(x, p/2) * x;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pow(3,4));
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,38 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
+
+    private static void arrayListTest() {
+        List<String> list = new ArrayList<>(2);
+        list.add("1");
+        list.add("2");
+        System.out.println(list.size());
+        Iterator<String> iterator = list.iterator();
+        synchronized (iterator){ // 多线程时在iterator对象上加锁
+            while (iterator.hasNext()) {
+                String item = iterator.next();
+                if("2".equals(item)){
+                    iterator.remove();
+                }
+            }
+        }
+        System.out.println(list.size());
+    }
+
+    private static void mapIteratorTest() {
+        HashMap<String, String> map = new HashMap<>(2);
+        map.put("one", "o my god");
+        map.put("two", "yes baby");
+        map.forEach((s, s2) -> System.out.println(s + "/" + s2));
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("hello World");
+        mapIteratorTest();
     }
 }
